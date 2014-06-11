aaedd4efc7305a78a92f7edd254ae8c34459cb1c
Devaev Maxim <mdevaev@gmail.com>
2014-06-11 20:13:19 +0400

    Version bump: 0.12

--- raava/const.py
+++ raava/const.py
@@ -1,5 +1,5 @@
 ##### Public constants #####
-VERSION_TUPLE = (0, 11)
+VERSION_TUPLE = (0, 12)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 UPSTREAM_URL = "https://github.com/yandex-sysmon/raava"
